d44a7247fa8d14bf8710f593c55f6fc1fdf40061
DBStirling <davidstirling17@gmail.com>
2024-09-28 01:41:19 -0400

    conditional showModal logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 function App() {
-
+  const [showModal, setShowModal] = useState(false) // default value = false
   return (
     <>
       <Main/>
-      <SideBar/>
+      {showModal && ( // conditional statement, if showModal is true, it opens sideBar
+        <SideBar/>
+      )}
       <Footer/>
     </>
   )
