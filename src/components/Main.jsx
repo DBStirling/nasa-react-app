f2d9725e6521fc3409ccd675ce1481eefd39302b
DBStirling <davidstirling17@gmail.com>
2024-09-28 01:14:30 -0400

    initial commit + basic styling and structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <div>
+            <div className="imgContainer">
+                <img className="bgImage" src="mars.png" alt="pretty demo pic of mars"></img>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+        <div className="sidebar">
+            <h2>The Baron but Beautiful Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>gbyberflb evev geg cgeivg ;evievl bebhv livhjwc</p>
+            </div>
+        </div>
+    )
+}
