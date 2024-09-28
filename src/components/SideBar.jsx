5cb0ead1cd720f93b679e870f9ea47e535948e2f
DBStirling <davidstirling17@gmail.com>
2024-09-28 01:34:51 -0400

    basic stylng 90% done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,16 @@
 export default function SideBar() {
     return (
         <div className="sidebar">
-            <h2>The Baron but Beautiful Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p>gbyberflb evev geg cgeivg ;evievl bebhv livhjwc</p>
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Baron but Beautiful Martian Landscape</h2>
+                <div>
+                    <p>Description</p>
+                    <p>gbyberflb evev geg cgeivg ;evievl bebhv livhjwc</p>
+                </div>
+                <button className="closeButton">
+                    <i className="fa-solid fa-circle-chevron-right"></i>
+                </button>
             </div>
         </div>
     )
